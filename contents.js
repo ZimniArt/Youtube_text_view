3e4d68c40df1510976513d7d1293846dbfdeccf8
zimniart-admin <zimni.art@gmail.com>
2025-12-13 21:29:47 +0300

    текст отображается на ютуб странице

--- contents.js
+++ contents.js
@@ -1,8 +1,20 @@
 console.log("test extension_start");
-const element = document.querySelectorAll(".yt-lockup-metadata-view-model__text-container");
+
+let textListContainer = document.getElementById("my-text-list");
+
+if(!textListContainer){
+    textListContainer = document.createElement("div");
+    textListContainer.id = "my-text-list";
+    textListContainer.style.fontSize = "16px";
+    textListContainer.style.padding = "10px";
+    textListContainer.style.backgroundColor ="#000";
+    textListContainer.style.color =  "#fff";
+    textListContainer.style.flex = "0 0 100%";
+    
+}
+
 
 const titleList = new Set();
-
 const observer = new MutationObserver(function observer(){
     const videos = document.querySelectorAll(".yt-lockup-metadata-view-model__text-container");
     videos.forEach(
@@ -11,10 +23,24 @@ const observer = new MutationObserver(function observer(){
             
             if (!title) return;
             if(titleList.has(title)) return;
-
             titleList.add(title);
-
             if(title) console.log(title);
+            
+            //adding to container
+            const div = document.createElement("div");
+            div.textContent = title;
+            div.style.marginBottom = "5px";
+            
+            let listHome = document.querySelector(".style-scope ytd-page-manager");
+            textListContainer.appendChild(div);
+            if (listHome){
+                listHome.prepend(textListContainer);
+            }
+            else{
+                console.log(listHome+": listHome");
+            }
+            
+            
         }
     )
 });
