2c32974bf580d629a3e02af332cd0a05d18ea738
zimniart-admin <zimni.art@gmail.com>
2025-12-13 17:18:46 +0300

    log text names

--- contents.js
+++ contents.js
@@ -1,3 +1,21 @@
-console.log("test extension");
-const element = document.querySelectorAll("yt-thumbnail-view-model");
-//element.hide;
+console.log("test extension_start");
+const element = document.querySelectorAll(".yt-lockup-metadata-view-model__text-container");
+
+const titleList = new Set();
+
+const observer = new MutationObserver(function observer(){
+    const videos = document.querySelectorAll(".yt-lockup-metadata-view-model__text-container");
+    videos.forEach(
+        function(video){
+            const title = video.querySelector(".yt-lockup-metadata-view-model__title")?.textContent?.trim();
+            
+            if(titleList.has(title)) return;
+            titleList.add(title);
+
+            if(title) console.log(title);
+        }
+    )
+});
+
+observer.observe(document.body, {childList: true, subtree: true});   
+console.log("test extension_end");
