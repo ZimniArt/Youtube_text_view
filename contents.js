c90b5c54f798132786d0b0f8a75e8e27494b88ef
zimniart-admin <zimni.art@gmail.com>
2025-12-14 23:05:54 +0300

    sometimes works

--- contents.js
+++ contents.js
@@ -4,6 +4,13 @@ let otherPage = "other";
 let videoList = "my-video-list";
 let pageManager = "ytd-page-manager";
 
+//this specific youtube event firing at mutation points
+window.addEventListener("yt-navigate-finish", handlePage);
+
+let currentPage = null;
+let newPage = null;
+
+let observer = null;
 
 function getPageType(){
     if(location.pathname === "/watch") return watchPage;
@@ -12,21 +19,23 @@ function getPageType(){
 }
 
 function handlePage(){
-    const page = getPageType();
-    if(page ===watchPage){
-        HandleWatchPage();
+
+    newPage = getPageType();
+    if(newPage===currentPage) return;
+    currentPage=newPage;
+    if(observer){
+        StopObserver();
     }
-    if(page ===homePage){
-        HandleHomePage();
-    }
-    if(page ===otherPage){
-        HandeleOtherPage();
-    }
+
+    if(newPage ===watchPage) HandleWatchPage();
+    if(newPage ===homePage)HandleHomePage();
+    if(newPage ===otherPage)HandeleOtherPage();
 }
 handlePage();
 
 function HandleHomePage(){
-    console.log("im at HomePage");
+
+    console.log("going to Home page");
 
     let videoListContainer = document.getElementById(videoList);
     
@@ -41,74 +50,99 @@ function HandleHomePage(){
         
     }
     
-    
-    
     waitForPageManager( (pageManager)=>{
         if (!videoListContainer.isConnected) {
             pageManager.prepend(videoListContainer);
         }
     });
     
-    
-    const titleList = new Set();
-    
-    const observer = new MutationObserver(() => {
-       
-        const videos = document.querySelectorAll(".yt-lockup-metadata-view-model__text-container");
-    
-        videos.forEach(video => {
-            const titleLink = video.querySelector(".yt-lockup-metadata-view-model__title");
-            if (!titleLink) return;
-    
-            const title = titleLink.textContent?.trim();
-            const href = titleLink.getAttribute("href");
-    
-            if (!title || !href) return;
-            if (titleList.has(title)) return;
-    
-            titleList.add(title);
-    
-            const link = document.createElement("a");
-            link.textContent = title;
-            link.href = href;
-            link.target = "_self";
-    
-            link.style.display = "block";
-            link.style.color = "#fff";
-            link.style.textDecoration = "none";
-            link.style.marginBottom = "6px";
-            link.style.cursor = "pointer";
-    
-            link.addEventListener("mouseenter", () => {
-                link.style.textDecoration = "underline";
-            });
-    
-            link.addEventListener("mouseleave", () => {
-                link.style.textDecoration = "none";
-            });
-    
-            videoListContainer.appendChild(link);
-        });
+    const titleList = new Set();  
+    videoListContainer.innerHTML = "";
+
+    AddvideosToList(videoListContainer, titleList);
+
+    observer = new MutationObserver(() => {
+        AddvideosToList(videoListContainer, titleList);
     });
     
-    
     observer.observe(document.body, {childList: true, subtree: true});   
-
+    
+    
+    
 }    
 
-//functions
+function AddvideosToList(videoListContainer, titleList){
+
+    const videos = document.querySelectorAll(".yt-lockup-metadata-view-model__text-container");
+
+    videos.forEach(video => {
+        const titleLink = video.querySelector(".yt-lockup-metadata-view-model__title");
+        if (!titleLink) return;
+
+        const title = titleLink.textContent?.trim();
+        const href = titleLink.getAttribute("href");
+
+        if (!title || !href) return;
+        if (titleList.has(title)) return;
+
+        titleList.add(title);
+
+        const link = document.createElement("a");
+        link.textContent = title;
+        link.href = href;
+        link.target = "_self";
+
+        link.style.display = "block";
+        link.style.color = "#fff";
+        link.style.textDecoration = "none";
+        link.style.marginBottom = "6px";
+        link.style.cursor = "pointer";
+
+        link.addEventListener("mouseenter", () => {
+            link.style.textDecoration = "underline";
+        });
+
+        link.addEventListener("mouseleave", () => {
+            link.style.textDecoration = "none";
+        });
+
+        videoListContainer.appendChild(link);
+    });
+}
+function HandleWatchPage(){
+    console.log("going to Watch page");
+    
+    observer = new MutationObserver(()=>{
+        const removeBlock = document.querySelector(".style-scope ytd-watch-next-secondary-results-renderer");
+        if (removeBlock) {
+            removeBlock.remove();
+        }
+        
+    })
+    observer.observe(document.body, {childList: true, subtree: true});
+}
+
+function StopObserver(){
+    if (!observer) return;
+    console.log(" observer disconected");
+    observer.disconnect();
+    observer = null;
+}
 function waitForPageManager(callback) {
-    const obs = new MutationObserver(() =>{
+
+    const existing = document.querySelector(pageManager);
+    if (existing) {
+        callback(existing);
+        return;
+    }
+
+    const obs = new MutationObserver(() => {
         const pageManager_exist = document.querySelector(pageManager);
-        if(pageManager_exist){
+        if (pageManager_exist) {
             obs.disconnect();
             callback(pageManager_exist);
         }
-    })
-
-    obs.observe(document.documentElement, {
-        childList: true,
-        subtree: true
     });
-}
 
+    obs.observe(document.documentElement, { childList: true, subtree: true });
+}
